4b96fa11407d0693c185f86b6fb9759b9032d6d7
Robertl231 <rloughran67@gmail.com>
2021-08-12 16:53:09 +0000

    added review page basic setup. combined cards and collapsible from materialize for a more appealing look

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right", draggable: "true"});
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+    $('.collapsible').collapsible();
   });
